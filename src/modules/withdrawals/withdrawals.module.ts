c49ae159fabc18eb1ac9df560dad0c6f503aaea4
jhuaynates <jhuaynatesc@gmail.com>
2026-07-28 00:09:44 -0500

    implementado el pago

---
+++ src/modules/withdrawals/withdrawals.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WithdrawalRequest } from './entities/withdrawal-request.entity';
+import { WithdrawalsService } from './withdrawals.service';
+import { WithdrawalsController, AdminWithdrawalsController } from './withdrawals.controller';
+import { WalletsModule } from '../wallets/wallets.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([WithdrawalRequest]),
+    WalletsModule,
+    NotificationsModule,
+  ],
+  controllers: [WithdrawalsController, AdminWithdrawalsController],
+  providers: [WithdrawalsService],
+  exports: [WithdrawalsService],
+})
+export class WithdrawalsModule {}
